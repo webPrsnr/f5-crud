b56764aab51b6f3379cf6c648af3701d33b6fa9f
William <webuntermench@protonmail.com>
2024-04-10 23:00:43 +0300

    Add buttons route handler

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,9 @@ import './bootstrap'
 import jQuery from 'jquery'
 
 window.$ = jQuery
+
+$(document).on('click', 'button', function () {
+  const url = this.dataset.route
+  if (url)
+    window.location = url
+})
